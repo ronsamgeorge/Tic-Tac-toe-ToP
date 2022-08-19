abeb28dabd9fd1e37146bd7bf10058302aeaf5c4
Ronald George <ronaldsamgeorge@gmail.com>
2022-08-20 01:54:08 +0530

    Update the display with the choice of the user

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ const Player = (name, playerSign) => {
 };
 
 
-// Gameboard will have the array and will check if the input is at a valid position
+//Gameboard will have the array and will check if the input is at a valid position
 const gameBoard = (() => {
 
     const gameBoardArray = new Array(9);
 
-    //checkInputPosition will check if the slot is preFilled or not;
+    //checkInputPosition will check if the slot is preFilled; 
     const checkInputPosition = (gridIndex) => {
-        if(gameBoardArray[gridIndex] === undefined){
-            return true;
+        if(gameBoardArray[gridIndex] === undefined){   
+            return true;            // if the index value has undefined as the value it suggest that the input div is valid
         }
         return false;
     }; 
@@ -28,18 +28,22 @@ const gameBoard = (() => {
         }
         
         gameBoardArray[gridIndex] = player.getPlayerSign();
-        console.log(gameBoardArray);
-        //DisplayController(gameBoardArray);
+        // console.log(gameBoardArray);
+        boardDisplayController.updateIndex(gridIndex,player);
     };
 
     return {updateGameBoardArray};
-
-
 })();
 
 
-// Modules Accept the gameboardArray and display the updates on the UI
-const DisplayController = (gridIndex) => ({
+//Modules Accept the valid grid index and update the grid UI with the player entry
+const boardDisplayController = (() => {
+    const updateIndex = (grindIndex,player) => {
+        let divDisplay = document.querySelector(`div[data-key="${grindIndex}"]`);  //uses the data-key custom data attribute select the div in the UI
+        divDisplay.textContent = player.getPlayerSign();
+    };
+
+    return {updateIndex};
 
 })();
 
@@ -60,6 +64,20 @@ const gameFlow = ((gameBoardArray) => {
     return {}; 
 })();
 
+const calculator = (() => {
+
+    const add = (a, b) => a + b;
+    const sub = (a, b) => a - b;
+    const mul = (a, b) => a * b;
+    const div = (a, b) => a / b;
+    return {
+      add,
+      sub,
+      mul,
+      div,
+    };
+  })();
+
 
 const testFunc = (index) =>{
     console.log(index);
@@ -72,6 +90,7 @@ let currentPlayer = player1;
 const divClicked = document.querySelectorAll(".game-board-divs");
 
 divClicked.forEach(div => div.addEventListener('click', function(){
+
     gameBoard.updateGameBoardArray(this.dataset.key,currentPlayer);
 }));
 
