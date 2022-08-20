30e9130508826dec3002136c1ca395894812700f
Ronald George <ronaldsamgeorge@gmail.com>
2022-08-20 13:47:33 +0530

    Add Game win and tie Conditions

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ const gameBoard = (() => {
         return false;
     }; 
 
-
     const updateGameBoardArray = (gridIndex, player) =>{
         
         if(!checkInputPosition(gridIndex)){ 
@@ -30,8 +29,6 @@ const gameBoard = (() => {
         gameBoardArray[gridIndex] = player.getPlayerSign();                         //updates array index with the player sign;
         boardDisplayController.updateIndex(gridIndex,player);
         gameFlow.gameLogic(gameBoardArray);
-
-
     };
 
     return {updateGameBoardArray};
@@ -50,7 +47,10 @@ const boardDisplayController = (() => {
 })();
 
 
-const gameFlow = ((gameBoardArray) => {
+const gameFlow = (() => {
+
+
+    let gameTurnCount = 0;
 
     //Keeps track of the player turn/rotation;
     const playerTurns = () => {
@@ -61,16 +61,72 @@ const gameFlow = ((gameBoardArray) => {
         currentPlayer = player1;    
     };
 
-    const checkResult = (gameBoardArray) => {
-        if(checkVertical){} 
+    const checkHorizontal = (gameBoardArray) => {
+        if((gameBoardArray[0] === gameBoardArray[1] && gameBoardArray[1] === gameBoardArray[2] && gameBoardArray[1] !== undefined)  ||
+            (gameBoardArray[3] === gameBoardArray[4] && gameBoardArray[4] === gameBoardArray[5] && gameBoardArray[4] !== undefined) ||
+            (gameBoardArray[6] === gameBoardArray[7] && gameBoardArray[7] === gameBoardArray[8] && gameBoardArray[7] !== undefined) ) {
+                return true;
+            }
+
+        return false;
     };
 
-    const gameLogic = (gameboardArray) => {
-        playerTurns();
-        console.log(currentPlayer);
+
+    const checkVertical = (gameBoardArray) => {
+        if((gameBoardArray[0] === gameBoardArray[3] && gameBoardArray[3] === gameBoardArray[6] && gameBoardArray[3] !== undefined) ||
+            (gameBoardArray[1] === gameBoardArray[4] && gameBoardArray[4] === gameBoardArray[7] && gameBoardArray[4] !== undefined ) ||
+            (gameBoardArray[2] === gameBoardArray[5] && gameBoardArray[5] === gameBoardArray[8] && gameBoardArray[5] !== undefined)) {
+                return true;
+            }
+
+        return false;
     };
 
-    return {gameLogic}; 
+
+    const checkDiagonal = (gameBoardArray) => {
+        if((gameBoardArray[0] === gameBoardArray[4] && gameBoardArray[4] === gameBoardArray[8]  && gameBoardArray[4] !== undefined) ||
+            (gameBoardArray[2] === gameBoardArray[4] && gameBoardArray[4] === gameBoardArray[6] && gameBoardArray[4] !== undefined)) {
+                return true;
+            }
+
+        return false;
+    };
+
+
+    const checkWinCon = (gameBoardArray) => {
+        if(checkVertical(gameBoardArray) || checkHorizontal(gameBoardArray) || checkDiagonal(gameBoardArray)){
+            
+            return true;
+        } 
+
+        return false;
+    };
+
+
+    const checkTieCon = () => {
+        if (gameTurnCount === 9){
+            return true;
+        }
+        return false;
+    };
+    
+
+    const gameLogic = (gameBoardArray) => {
+
+        gameTurnCount++;
+        if(checkWinCon(gameBoardArray)){
+            console.log(`${currentPlayer.getName()} won!!`);
+            return;
+        };
+
+        if(checkTieCon()){
+            console.log("Tie Game");
+        };
+
+        playerTurns(); 
+    };
+
+    return {gameLogic, gameTurnCount}; 
 })();
 
 
